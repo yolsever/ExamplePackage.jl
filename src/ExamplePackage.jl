c59b0cca19bbe5431110f0c1eeb9fe7a50d158f6
Kaan Yolsever <kaanyolsever@gmail.com>
2018-11-25 21:05:20 -0800

    asd

--- src/ExamplePackage.jl
+++ src/ExamplePackage.jl
@@ -2,4 +2,14 @@ module ExamplePackage
 
 greet() = print("Hello World!")
 
+using Expectations, Distributions
+
+function foo(μ = 1., σ = 2.)
+    d = Normal(μ, σ)
+    E = expectation(d)
+    return E(x -> sin(x))
+end
+
+export foo
+
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,6 @@
 using ExamplePackage
 using Test
 
-@testset "ExamplePackage.jl" begin
-    # Write your own tests here.
-end
+@test foo() == 0.11388071406436832
+@test foo(1, 1.5) == 0.2731856314283442
+@test_broken foo(1, 0) # tells us this is broken
